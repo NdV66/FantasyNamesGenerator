1eb890f0da6a3c03fc2c328ede59e1fc7cc50983
Marta Zażlak <marta.zazlak@hotmail.com>
2019-02-12 19:08:19 +0100

    init

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+
+export const PageJumbotron = () => <Jumbotron className='page-jumbotron text-center text-light'>
+        <h1>Fantasy names generator</h1>
+        <h2>beacuse we love Role Plaing Games so much!</h2>
+        <Button color='primary' className='mt-3'>Try it!</Button>
+</Jumbotron>;
